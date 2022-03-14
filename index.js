fc76a6bd4eecfbf977f3ce6376504aa06f713ac0
Joshua <j22yosh@gmail.com>
2022-03-14 00:27:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b){
+return (a + b)
+}
+function subtract(a, b){
+return (a-b)
+}
+function multiply(a, b){
+return (a*b)
+}
+function divide(a, b){
+return (a/b)
+}
+function increment (n){
+    return (n+1)
+}
+function decrement (n){
+    return (n-1)
+}
+function makeInt(n)
+{return parseInt(n, 10); // I've tried with and without the radix and it still doesn't "assume" base 10 on the test.
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+    
+
+}
+
+
